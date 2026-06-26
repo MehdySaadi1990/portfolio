c399df4645c2d2e5a2589743644489c86ea91991
Mehdy Saadi <mehdy.saadi.dk@gmail.com>
2026-06-26 05:39:57 +0200

    commit

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -4,6 +4,7 @@ const sendEmail = async (email, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
+      service: 'gmail',
       port: process.env.PORT_EMAIL,
       secure: false,
       auth: {
